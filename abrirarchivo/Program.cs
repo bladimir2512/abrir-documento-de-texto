8d6fe242b8c92d0c30a42d1b2ca6cc5f42537854
bladimir2512 <140455406+bladimir2512@users.noreply.github.com>
2023-08-07 10:11:48 -0600

    Add files via upload

---
+++ abrirarchivo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace abrirarchivo
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\Marvi\OneDrive\Escritorio\abrirarchivo\bin\Debug\net6.0\archivo.txt");
+            Console.WriteLine("\t{0}", text);
+            Console.ReadLine();
+            Console.ReadKey();
+             
+        }
+       
+        
+    }
+}
